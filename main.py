b5d1b36bb534a8f440ee4cf530d6851a80505971
math-pixel <59537610+math-pixel@users.noreply.github.com>
2023-11-08 21:42:16 +0100

    add three first page : select save, pseudo and level

--- main.py
+++ main.py
@@ -2,12 +2,66 @@ from src.scripts import pageManager as pageManager
 from src.scripts import menuSelection
 import json
 from src.scripts import tools as tool
+import time
+
+
+idPageSave = 0
+pseudo = ""
+level = 0 # 0 : easy | 1 : medium | 2 : hard
 
 def start():
-    readPage(0)
+    global pseudo, level
+
+    # get the current id of page in function of save
+    idPageSave = savePage()
+
+    pseudo = getName()
+
+    level = getLevel()
+
+    readPage(idPageSave)
+
+def getLevel():
+    tool.clearConsole()
+
+    levelChoices = [
+            {
+                "text": "Easy",
+                "redirection_id": "0"
+            },
+            {
+                "text": "Normal",
+                "redirection_id": "1"
+            },
+            {
+                "text": "Hard",
+                "redirection_id": "2"
+            }
+        ]
+
+    pageManager.writeQuestion(question="Choisis ton niveau de difficulté ", color="white", timeout=0.01)
+    return pageManager.writeChoices(typeChoice="arrow", choices=levelChoices, question="Choisis ton niveau de difficulté ")
+
+def getName():
+    tool.clearConsole()
+
+    pageManager.writeQuestion(question="C'est quoi ton joli ptit nom ?", color="white", timeout=0.01)
+    return input(">")   
+
+def savePage():
+
+    # TODO get save existing
+    nbSave = []
+
+    if len(nbSave) > 0:
+        # TODO choice between empty slot or used slot
+        print("choisis ta souvegarde ;)")
+        return "l'id qui a ete choisis"
+    else:
+        pageManager.writeQuestion(question="creation d'une nouvelle sauvegarde ...", color="white", timeout=0.05)
+        return 0
 
 def readPage(idPage):
-
     tool.clearConsole()
 
     pageManager.setPath("./src/assets/structure.json")
